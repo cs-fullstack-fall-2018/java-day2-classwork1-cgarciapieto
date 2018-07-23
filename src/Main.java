6cf44c45b5c84cca4114a951622aa6ea39281b55
Carlos Garcia <cgarciapieto@gmail.com>
2018-07-23 11:44:49 -0500

    java-day2-classwork1-cgarciapieto.iml

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner consoleInput = new Scanner(System.in);
+
+        String thoughts = "";
+        System.out.println("How awesome is Kenn and Kevin?");
+        thoughts = consoleInput.nextLine();
+        while (!thoughts.equals("very awesome"));
+        System.out.println("Wrong");
+
+        thoughts =consoleInput.nextLine();
+
+
+        }
+    }
